7dc66fbad07ac4bb6192ab49e6e64d144f3095e2
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-02-10 19:13:36 +0530

    Fetch Movie on page Load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,12 +8,8 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError ] = useState(null);
 
-  async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-
-    async function fetchMoviesHandler() {
-      setIsLoading(true); 
+  const fetchMoviesHandler = useCallback(async () =>{
+    setIsLoading(true); 
     setError(null); 
 
     try{
@@ -38,8 +34,12 @@ function App() {
       setTimeout(fetchMoviesHandler,5000)
     }
     setIsLoading(false);
-  }
+  
+},[]);
 
+useEffect(()=>{
+  fetchMoviesHandler();
+}, [fetchMoviesHandler])
 
   let content = <p>Found no movies</p>
 
@@ -53,7 +53,6 @@ function App() {
 
   if(isLoading)  {
     content =<p>Loading...</p>
-  }
   }
 
   return (
